b2419e4818f058a35bacc83d0ebc9dc2c4fefd04
keipala <keipalahniuk@gmail.com>
2023-04-26 11:20:02 +0200

    add filter on keystroke

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,6 +6,7 @@ import {
     Form,
     redirect,
     useNavigation,
+    useSubmit,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 import { useEffect } from "react";
@@ -25,6 +26,7 @@ export async function action() {
 export default function Root() {
   const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
 
     useEffect(() => {
       document.getElementById("q").value = q;
@@ -43,6 +45,9 @@ export default function Root() {
                 type="search"
                 name="q"
                 defaultValue={q}
+                onChange={(event) => {
+                submit(event.currentTarget.form);
+              }}
               />
               <div
                 id="search-spinner"
