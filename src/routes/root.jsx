cf651c4a6f2be587a993a81d0ed1773306823298
keipala <keipalahniuk@gmail.com>
2023-04-26 10:41:18 +0200

    Add selected navigation link

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,7 @@
 import { 
     Link, 
     Outlet,
+    NavLink,
     useLoaderData,
     Form,
     redirect,
@@ -52,7 +53,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -61,7 +71,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                    </NavLink>
                 </li>
               ))}
             </ul>
